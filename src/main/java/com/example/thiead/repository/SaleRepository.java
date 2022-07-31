3d72947df04f21799b38c1d32651cc5c7ead3694
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-07-31 17:33:59 +0700

    bai thi EAD

---
+++ src/main/java/com/example/thiead/repository/SaleRepository.java
@@ -0,0 +1,8 @@
+package com.example.thiead.repository;
+
+import com.example.thiead.entity.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale,Long> {
+
+}
